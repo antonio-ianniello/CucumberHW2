ab0e8603c78469a9ad2e62be6e482d536720287c
anton <anton@192.168.1.59>
2020-03-19 17:20:47 +0100

    I setup Cucumber on project

---
+++ diadiaHW2CucumberTest/src/test/java/diadiaHW2CucumberTest/RunTester.java
@@ -0,0 +1,12 @@
+package diadiaHW2CucumberTest;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:testHw2/HW2.feature",glue="diadiaHW2CucumberTest",plugin= {"junit:target/junit-xml-report.xml"})
+public class RunTester {
+
+}
---
+++ diadiaHW2CucumberTest/src/test/java/diadiaHW2CucumberTest/StepDefinition.java
@@ -0,0 +1,41 @@
+package diadiaHW2CucumberTest;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepDefinition {
+	
+	@Given("^I am in atrio$")
+	public void i_am_in_atrio() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^I start the game$")
+	public void i_start_the_game() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^digit command(\\d+)\"([^\"]*)\" and command(\\d+)\"([^\"]*)\"$")
+	public void digit_command_and_command(int arg1, String arg2, int arg3, String arg4) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I am in the Winner Room$")
+	public void i_am_in_the_Winner_Room() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I win the game$")
+	public void i_win_the_game() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+
+}
